22b5e5e837aa642320c260ee436e72d71f844e34
Fredersen <naim.jhuboo@gmail.com>
2022-03-23 23:48:11 +0100

    twitch

---
+++ src/api.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+let api = axios.create({
+    headers: {
+      'Client-ID' : 'npi57yhbqg00cd1a3fp5h2k0zaqm19',
+      'Authorization' : 'Bearer 1yijel0r4kj4k6eunj87pwn618fiek'
+    }
+  })
+  
+
+export default api;
